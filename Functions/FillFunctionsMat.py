258aaf0197efb966965ecf82114d57b454286616
eachingateb <eachingateb@unal.edu.co>
2024-10-23 16:37:22 -0500

    Comeback

--- Functions/FillFunctionsMat.py
+++ Functions/FillFunctionsMat.py
@@ -1,18 +1,21 @@
 import os
 from ReadSummary import *
 from CleanSentence import *
-def FillFunctionsMat(FunctionsFolder,FunctionsMat):
+def FillFunctionsMat(FunctionsFolder,FunctionsMat,FunctionsListDF):
     for File in os.listdir(FunctionsFolder):
         FileLoc=FunctionsFolder+'/'+File
         FunctionName=File.replace('.py','')
         FunctionID=int(FunctionsListDF['ID'][FunctionName])
-        Text=ReadSummary(FileLoc)
-        for line in Text:
-            function=CleanSentence(line,startKey="from ",endKey=" import *")
-            if function!=0:
-                try:
-                    CalledFunctionID=int(FunctionsListDF['ID'][function])
-                    FunctionsMat[FunctionID,CalledFunctionID]=1
-                except:
-                    print(function)
+        try:
+            Text=ReadSummary(FileLoc)
+            for line in Text:
+                function=CleanSentence(line,startKey="from ",endKey=" import *")
+                if function!=0:
+                    try:
+                        CalledFunctionID=int(FunctionsListDF['ID'][function])
+                        FunctionsMat[FunctionID,CalledFunctionID]=1
+                    except:
+                        print(function)
+        except:
+            print(File)
     return FunctionsMat
--- Functions/ReturnReturner.py
+++ Functions/ReturnReturner.py
@@ -1,18 +1,21 @@
 import os
 from ReadSummary import *
 from CleanSentence import *
-def ReturnReturner(FunctionsFolder):
+def ReturnReturner(FunctionsFolder,FunctionsListDF):
     FunctionOutputs=[]
     for File in os.listdir(FunctionsFolder):
         FileLoc=FunctionsFolder+'/'+File
         FunctionName=File.replace('.py','')
         FunctionID=int(FunctionsListDF['ID'][FunctionName])
-        Text=ReadSummary(FileLoc)
-        for line in Text:
-            output=CleanSentence(line,startKey="return ",endKey='\n')
-            if output!=0:
-                try:
-                    FunctionOutputs.append([FunctionName,[output]])
-                except:
-                    print(output)
+        try:
+            Text=ReadSummary(FileLoc)
+            for line in Text:
+                output=CleanSentence(line,startKey="return ",endKey='\n')
+                if output!=0:
+                    try:
+                        FunctionOutputs.append([FunctionName,[output]])
+                    except:
+                        print(output)
+        except:
+            print(File)
     return FunctionOutputs
